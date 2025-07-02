61118c4a38f680b5ae13ed3aa2ee84be62ade287
nirdhum <nirdhum.n@gmail.com>
2025-07-02 15:48:08 +0530

    Create Movie Card Layout

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 
-const MovieCard = ({movie}) => {
+const MovieCard = ({movie:
+    {
+        title,
+        original_language,
+        poster_path,
+        release_date,
+    }
+}) => {
   return (
-    <div>
-        <p key={movie.id} className="text-white">{movie.title}</p>
-        <p key={movie.id} className="text-white">{movie.overview}</p>
+    <div className='movie-card'>
+        <img src={poster_path?`https://image.tmdb.org/t/p/w500/${poster_path}`:'./no-movie.png'} alt="" />
+        <p className="text-white">{title}</p>
+        <p className="text-white">{release_date}</p>
+        <p className="text-white">{original_language}</p>
     </div>
   )
 }
